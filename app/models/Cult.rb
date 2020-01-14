9437102a60f1e56a36b1d0cab865eac90f4ff3d8
Derick Castillo <derick.castillo@flatironschool.com>
2020-01-14 10:35:56 -0600

    Finished code

---
+++ app/models/Cult.rb
@@ -0,0 +1,101 @@
+class Cult
+
+    attr_accessor :name, :location, :founding_year, :slogan, :minimum_age
+  
+    @@all = []
+  
+    def initialize(name, location, founding_year, slogan, minimum_age)
+      @name = name
+      @location = location
+      @founding_year = founding_year
+      @slogan = slogan
+      @minimum_age = minimum_age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.find_by_name(name)
+      self.all.find do |cult|
+        cult.name == name
+      end
+    end
+  
+    def self.find_by_location(location)
+      self.all.select do |cult|
+        cult.location == location
+      end
+    end
+  
+    def self.find_by_founding_year(year)
+      self.all.select do |cult|
+        cult.founding_year == year
+      end
+    end
+  
+    def self.least_popular
+      self.all.min_by do |cult|
+        cult.followers.length
+      end
+    end
+  
+    def self.cult_locations
+      locations = {}
+      self.all.each do |cult|
+        if !locations[cult.location]
+          locations[cult.location] = 1
+        else
+          locations[cult.location] += 1
+        end
+      end
+      return locations
+    end
+  
+    def self.most_common_location
+      location = cult_locations.max_by do |location, count|
+        count
+      end
+      location[0]
+    end
+  
+    def blood_oaths
+      BloodOath.all.select do |bloodoath|
+        bloodoath.cult == self
+      end
+    end
+  
+    def followers
+      blood_oaths.map do |bloodoath|
+        bloodoath.follower
+      end
+    end
+  
+    def cult_population
+      followers.length
+    end
+  
+    def recruit_follower(follower)
+      if follower.age >= self.minimum_age
+        BloodOath.new(follower, self)
+      else
+        puts "Sorry, #{follower.name} is not yet old enough to join this cult."
+      end
+    end
+  
+    def average_age
+      total_age = followers.reduce(0) do | sum, follower |
+       sum + follower.age
+     end
+     total_age / followers.length
+    end
+  
+    def my_followers_mottos
+      followers.map do |follower|
+        follower.life_motto
+      end
+    end
+  
+  
+  end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,18 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+kitten = Cult.new("kitten", "Chicago", 2020, "meeeeoww")
+dog = Cult.new("dog", "New York", 1900, "oink oink")
+frog = Cult.new("frog", "Chicago", 2020, "moo moo")
+
+keenan = Follower.new("Keenan", 50, "never sleep")
+john = Follower.new("John", 5, "always sleep")
+nick = Follower.new("Nick", 50, "scar on my face")
+
+first_blood = BloodOath.new(kitten, keenan)
+second_blood = BloodOath.new(dog, john)
+kitten.recruit_follower(nick)
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
